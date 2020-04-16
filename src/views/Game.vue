71caef16f87730e4e9eeab71ca4d6440769c92ed
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-04-16 21:22:32 +0700

    improve user view

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -7,7 +7,7 @@
       <div class="row">
         <div>
           <div class="bg">
-          <button class="btn btn-danger console"  @click="move()">MOVE</button>
+          <img @click="move()" class="stick" src="../assets/gamepad.png" alt="playgame">
             <img class="board1" src="../assets/nagamerah.png" :style="`left:${leftBoard1}%`" />
             <img class="board2" src="../assets/nagaijo.png" :style="`left:${leftBoard2}%`" />
           </div>
@@ -17,13 +17,13 @@
             <h2>Players</h2>
             <ul class="w3-ul w3-card-4">
               <li class="w3-bar w3-bar-1">
-                <img src="https://www.w3schools.com/w3css/img_avatar2.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
+                <img src="../assets/user1.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
                 <div class="w3-bar-item mt-2">
                   <span>{{playerName}}</span>
                 </div>
               </li>
               <li class="w3-bar w3-bar-2">
-                <img src="https://www.w3schools.com/w3css/img_avatar5.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
+                <img src="../assets/user2.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
                 <div class="w3-bar-item mt-2">
                   <span>Other Player</span>
                 </div>
@@ -121,12 +121,12 @@ export default {
   animation: myfirst2 1s 1;
   animation-direction: normal;
 }
-.console {
-  border-radius: 100%;
+.stick {
   position: absolute;
   top: 250px;
   right: 180px;
-  height: 72px;
+  width: 80px;
+  cursor: pointer;
 }
 .board1 {
   left: 10%;
@@ -154,6 +154,7 @@ export default {
 }
 .w3-card-4, .w3-hover-shadow:hover{
   box-shadow: 0 4px 10px 0 rgba(0,0,0,0.2),0 4px 20px 0 rgba(0,0,0,0.19);
+  border-radius: 10px;
   width: 25vw;
 }
 .w3-ul {
